bc26019b9aea33ec5ff08d36abec777c19425078
valentin <valentin.escamilla@outlook.fr>
2017-12-14 10:52:32 +0100

    Add properties to Sandwich

--- Code/src/main/java/org/lpro/entity/Sandwich.java
+++ Code/src/main/java/org/lpro/entity/Sandwich.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.NamedQuery;
+import javax.validation.constraints.NotNull;
 
 /**
  *
@@ -22,5 +23,53 @@ public class Sandwich {
     @Id
     @GeneratedValue
     private long id;
+    
+    @NotNull
+    private String nom;
+    @NotNull
+    private String description;
+    @NotNull
+    private String type_pain;
+    private String img;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType_pain() {
+        return type_pain;
+    }
+
+    public void setType_pain(String type_pain) {
+        this.type_pain = type_pain;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
     
 }
