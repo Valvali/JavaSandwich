d90fcf5500d918042ebe5a88f6dd94c9a31d9f42
valentin <valentin.escamilla@outlook.fr>
2018-01-30 15:49:14 +0100

    Add create sandwich

--- Code/src/main/java/org/lpro/boundary/SandwichResource.java
+++ Code/src/main/java/org/lpro/boundary/SandwichResource.java
@@ -5,14 +5,20 @@
  */
 package org.lpro.boundary;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.validation.Valid;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -66,6 +72,21 @@ public Response getSandwichs(@QueryParam("type") String ptype) {
         this.sm.delete(id);
         return Response.status(Response.Status.NO_CONTENT).build();
     }
+    
+    
+    @POST
+    @ApiOperation(value = "Crée un sandwich", notes = "Crée une sandwich partir du JSON fourni")
+    @ApiResponses(value = {
+        @ApiResponse(code = 201, message = "Created"),
+        @ApiResponse(code = 500, message = "Internal server error")})
+    public Response newSandwich(@Valid Sandwich s, @Context UriInfo uriInfo) {
+        Sandwich newOne = this.sm.save(s);
+        long id = newOne.getId();
+        URI uri = uriInfo.getAbsolutePathBuilder().path("/"+id).build();
+        return Response.created(uri).build();
+    }
+  
+  
     
     
 
